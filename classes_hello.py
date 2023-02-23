0b70bf7e6ef375432482307b2e73b0c306fe907f
VloneDSb <kozachekivan60@gmail.com>
2023-02-23 13:16:56 +0300

    classes_hello update 2.0
    working!

--- classes_hello.py
+++ classes_hello.py
@@ -1,58 +1,17 @@
-class Solution:
-    '''Приветствие человек'''
-    def __init__(self):  #, name, surname, age
-        '''свойсвта по умолчанию'''
-        self.name = 'Ivan' 
-        self.surname =  'Ivanov' 
-        self.age =  14 
-    
-
-    '''Пока что не понял как реализовать'''
-    def hello(self, name, surname, age):
-            '''Берет аргументы и вставляет в строчку'''
-            print(f"Hello {name} {surname} {age} years old")
-
-
-
-
-
-people = Solution()
-people.hello('Dima', 'Petrov', 20)         #Hello Dima Petrov 20 years old
-people.hello('Dima', 'Petrov')             #Hello Dima Petrov 14 years old
-people.hello('Dima')                       #Hello Dima Ivanov 14 years old
-people.hello()                             #Hello Ivan Ivanov 14 years old
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # class Solution:
-
+#     '''Приветствие человек'''
 #     def __init__(self):  #, name, surname, age
-#         self.name =  'Ivan' 
+#         '''свойсвта по умолчанию'''
+#         self.name = 'Ivan' 
 #         self.surname =  'Ivanov' 
 #         self.age =  14 
     
-    
 
-#     def hello(self, name , surname, age):#, name , surname, age
-#         if people.hello():
+#     '''Пока что не понял как реализовать'''
+#     def hello(self, name, surname, age):
+#             '''Берет аргументы и вставляет в строчку'''
 #             print(f"Hello {name} {surname} {age} years old")
-#         elif 
+
 
 
 
@@ -62,3 +21,42 @@ people.hello()                             #Hello Ivan Ivanov 14 years old
 # people.hello('Dima', 'Petrov')             #Hello Dima Petrov 14 years old
 # people.hello('Dima')                       #Hello Dima Ivanov 14 years old
 # people.hello()                             #Hello Ivan Ivanov 14 years old
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution:
+
+    # def __init__(self):  #, name, surname, age
+    #     self.name = 'Ivan' 
+    #     self.surname = 'Ivanov' 
+    #     self.age = 14 
+
+
+
+    def hello(self, name = "Ivan" , surname = 'Ivanov', age = 14):#, name , surname, age
+        print(f"Hello {name} {surname} {age} years old")
+
+
+
+
+people = Solution()
+people.hello('Dima', 'Petrov', 20)         #Hello Dima Petrov 20 years old
+people.hello('Dima', 'Petrov')             #Hello Dima Petrov 14 years old
+people.hello('Dima')                       #Hello Dima Ivanov 14 years old
+people.hello()                             #Hello Ivan Ivanov 14 years old
