a2ea6cc1168bf6642289befb2145a01d924153d1
VloneDs <124910019+VloneDs@users.noreply.github.com>
2023-02-23 18:34:12 +0300

    update idk
    yeep x2 best of the best

--- classes_hello.py
+++ classes_hello.py
@@ -1,61 +1,14 @@
-# class Solution:
-#     '''Приветствие человек'''
-#     def __init__(self):  #, name, surname, age
-#         '''свойсвта по умолчанию'''
-#         self.name = 'Ivan' 
-#         self.surname =  'Ivanov' 
-#         self.age =  14 
-    
-
-#     '''Пока что не понял как реализовать'''
-#     def hello(self, name, surname, age):
-#             '''Берет аргументы и вставляет в строчку'''
-#             print(f"Hello {name} {surname} {age} years old")
-
-
-
-
-
-# people = Solution()
-# people.hello('Dima', 'Petrov', 20)         #Hello Dima Petrov 20 years old
-# people.hello('Dima', 'Petrov')             #Hello Dima Petrov 14 years old
-# people.hello('Dima')                       #Hello Dima Ivanov 14 years old
-# people.hello()                             #Hello Ivan Ivanov 14 years old
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Solution:
-
-    # def __init__(self):  #, name, surname, age
-    #     self.name = 'Ivan' 
-    #     self.surname = 'Ivanov' 
-    #     self.age = 14 
-
-
-
+    '''Приветствие с человеком'''
     def hello(self, name = "Ivan" , surname = 'Ivanov', age = 14):#, name , surname, age
+        '''
+            Есть уже встроенные нахвания, которые будут заменять пропущенные аргументы
+        '''
         print(f"Hello {name} {surname} {age} years old")
 
 
-
-
 people = Solution()
+
 people.hello('Dima', 'Petrov', 20)         #Hello Dima Petrov 20 years old
 people.hello('Dima', 'Petrov')             #Hello Dima Petrov 14 years old
 people.hello('Dima')                       #Hello Dima Ivanov 14 years old
